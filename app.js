45a44914550f1eb2b288452d9917a0cd4e839a9b
ShakzOloyed <bim0514@hotmail.co.uk>
2022-04-23 09:30:00 +0100

    add javascript section

--- app.js
+++ app.js
@@ -1,18 +1,60 @@
-const button = document.querySelector("#container");
+const openButton = document.querySelector("#Log-in");
+const closeButton = document.querySelector("#exist");
+const form = document.querySelector("#popup-form");
 
-const form = document.querySelector(".form");
-const close = document.querySelector("#close-btn");
 
-document.querySelector("#container").addEventListener
+openButton.addEventListener("click", () => {
+  document.querySelector("#popup-form").style.display = "block";
 
-button.addEventListener("click", () => {
-  form.classList.add("show")
-  // console.log("Button clicked");
+});  
+
+closeButton.addEventListener("click", () => {
+  document.querySelector("#popup-form").style.display = "none";
+
 });
 
-button.addEventListener("click", () => {
-  form.classList.add("show")
-  console.log("Button clicked");
-});
+
+const loginForm = document.querySelector("#Login-form");
+const email = document.querySelector("#email");
+const userName = document.querySelector("#user-name");
+const password = document.querySelector("#psw");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validateInputs();
+})
+
+
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+}
+
+if(userName === '') {
+  alert('Username cant be blank');
+}else{
+  alert('User name must be lowercase');
+}
+
+if(email === '') {
+  alert('Email can not be blank');
+ } else {
+   alert( 'Provide a valid email address');
+ }
+
+  if(password=== 'password.length>6') {
+    alert('Password not valid');
+  }else {
+    alert('Password must be at least 6 character');
+
+  }
+
+
+
+
 
 
